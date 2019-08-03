1feed24bfbb74e0c165205c1a73deedd08c37249
KID421 <suchild7@gmail.com>
2019-08-03 15:00:07 +0800

    運算子

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ var check = true;           // 布林值 true / false
 
 // 網頁文件.透過 ID 取得元素.插入 HTML = 值;
 document.getElementById("title").innerHTML = text;
+
+// 運算子
+var a = 10;
+var b = 5;
+var c = 0;
+// 等號右邊先運算再指定到左邊
+c = a + b;  
+c = a - b;
+c = a * b;
+c = a / b;
+c = a % b;
+
+document.getElementById("box1").innerHTML = c;
+
+var d = 1;
+var e = 1;
+var f = 0;
+
+f = d++;    // 先給值再遞增
+f = ++e;    // 先遞增再給值
+
+document.getElementById("box2").innerHTML = f;
